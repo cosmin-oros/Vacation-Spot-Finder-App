b4d02a3672c00f7c52170ba2180138c5b8bb7c52
cosmin-oros <oroscosmin09@gmail.com>
2022-08-11 17:35:18 +0300

    continents

--- app/src/main/java/cosmin/dev/travelapp/ui/screens/continents_package/ListOfContinents.kt
+++ app/src/main/java/cosmin/dev/travelapp/ui/screens/continents_package/ListOfContinents.kt
@@ -1,2 +1,13 @@
 package cosmin.dev.travelapp.ui.screens.continents_package
 
+object ListOfContinents{
+    fun getData(): ArrayList<ContinentsData>{
+        return arrayListOf(
+            /*ContinentsData(
+                imgRes =,
+                continentName = "Europe",
+                info = ""
+            )*/
+        )
+    }
+}
