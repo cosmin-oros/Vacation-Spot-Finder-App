2c1760c866221d02c1fb5dfcc99ead8509e67026
cosmin-oros <oroscosmin09@gmail.com>
2022-08-12 13:58:25 +0300

    cities

---
+++ app/src/main/java/cosmin/dev/travelapp/ui/screens/cities_package/CitiesData.kt
@@ -0,0 +1,7 @@
+package cosmin.dev.travelapp.ui.screens.cities_package
+
+data class CitiesData(
+    val imgRes: Int,
+    val cityName: String,
+    val info: String
+)
---
+++ app/src/main/java/cosmin/dev/travelapp/ui/screens/cities_package/ListOfCities.kt
@@ -0,0 +1,15 @@
+package cosmin.dev.travelapp.ui.screens.cities_package
+
+import cosmin.dev.travelapp.ui.screens.continents_package.ContinentsData
+
+object ListOfCities {
+    fun getData(): ArrayList<CitiesData>{
+        return arrayListOf(
+            /*CitiesData(
+                imgRes =,
+                cityName = "",
+                info = ""
+            )*/
+        )
+    }
+}
