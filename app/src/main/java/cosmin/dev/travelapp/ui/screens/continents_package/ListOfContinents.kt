3522aa798e124414be510396fc855eaf327211f2
cosmin-oros <oroscosmin09@gmail.com>
2022-08-13 19:28:23 +0300

    list of continents

--- app/src/main/java/cosmin/dev/travelapp/ui/screens/continents_package/ListOfContinents.kt
+++ app/src/main/java/cosmin/dev/travelapp/ui/screens/continents_package/ListOfContinents.kt
@@ -1,13 +1,40 @@
 package cosmin.dev.travelapp.ui.screens.continents_package
 
+import cosmin.dev.travelapp.R
+
 object ListOfContinents{
     fun getData(): ArrayList<ContinentsData>{
         return arrayListOf(
-            /*ContinentsData(
-                imgRes =,
+            ContinentsData(
+                imgRes = R.drawable.africa,
+                continentName = "Africa",
+                info = "54 countries"
+            ),
+            ContinentsData(
+                imgRes = R.drawable.asia,
+                continentName = "Asia",
+                info = "48 countries"
+            ),
+            ContinentsData(
+                imgRes = R.drawable.europe,
                 continentName = "Europe",
-                info = ""
-            )*/
+                info = "44 countries"
+            ),
+            ContinentsData(
+                imgRes = R.drawable.northamerica,
+                continentName = "North America",
+                info = "23 countries"
+            ),
+            ContinentsData(
+                imgRes = R.drawable.oceania,
+                continentName = "Oceania",
+                info = "14 countries"
+            ),
+            ContinentsData(
+                imgRes = R.drawable.southamerica,
+                continentName = "South America",
+                info = "12 countries"
+            )
         )
     }
 }
