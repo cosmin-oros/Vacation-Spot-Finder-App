ca56b2e276e0de68b0e772da8584c7aa65fe3a67
cosmin-oros <oroscosmin09@gmail.com>
2022-08-16 12:45:02 +0300

    list of cities update

--- app/src/main/java/cosmin/dev/travelapp/ui/screens/cities_package/ListOfCities.kt
+++ app/src/main/java/cosmin/dev/travelapp/ui/screens/cities_package/ListOfCities.kt
@@ -39,6 +39,41 @@ object ListOfCities {
                 country = "Albania",
                 continent = "Europe",
                 firstCity = 0
+            ),
+            CitiesData(
+                imgRes = R.drawable.algiers,
+                cityName = "Algiers",
+                country = "Algeria",
+                continent = "Africa",
+                firstCity = 1
+            ),
+            CitiesData(
+                imgRes = R.drawable.annaba,
+                cityName = "Annaba",
+                country = "Algeria",
+                continent = "Africa",
+                firstCity = 0
+            ),
+            CitiesData(
+                imgRes = R.drawable.oran,
+                cityName = "Oran",
+                country = "Algeria",
+                continent = "Africa",
+                firstCity = 0
+            ),
+            CitiesData(
+                imgRes = R.drawable.andorralavella,
+                cityName = "Andorra la Vella",
+                country = "Andorra",
+                continent = "Europe",
+                firstCity = 1
+            ),
+            CitiesData(
+                imgRes = R.drawable.pasdelacasa,
+                cityName = "Pas de la Casa",
+                country = "Andorra",
+                continent = "Europe",
+                firstCity = 0
             )
         )
     }
